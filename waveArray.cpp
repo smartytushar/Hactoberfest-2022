788f2603a62ec70a492cff9d7bdb07f4060fbd46
deepak9010 <deepak23072019@gmail.com>
2022-10-11 10:30:33 +0530

    create Wave Array.cpp

--- waveArray.cpp
+++ waveArray.cpp
@@ -11,7 +11,7 @@
     //Function to sort the array into a wave-like array.
     void convertToWave(int n, vector<int>& arr){
         
-        // Your code heregbfdsfdnbsdbb
+        // Your code heregbfdsfdnbsdbbsds
           if(n%2!=0){
            for(int i=0;i<n;i++){
                if(i%2==0 && i<n-1){
